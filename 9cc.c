4ecbb2f6b3155e6417ee0c3116dfedae81e04712
labocho <labocho@penguinlab.jp>
2019-06-07 14:20:49 +0900

    単項プラスと単項マイナス

--- 9cc.c
+++ 9cc.c
@@ -80,14 +80,22 @@ Node *term() {
   }
 }
 
+Node *unary() {
+  if (consume('+'))
+    return term();
+  if (consume('-'))
+    return new_node('-', new_node_num(0), term()); // - num -> 0 - num
+  return term();
+}
+
 Node *mul() {
-  Node *node = term();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*')) {
-      node = new_node('*', node, term());
+      node = new_node('*', node, unary());
     } else if (consume('/')) {
-      node = new_node('/', node, term());
+      node = new_node('/', node, unary());
     } else {
       return node;
     }
